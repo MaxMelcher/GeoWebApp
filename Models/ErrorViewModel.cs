53b4fb5194dfa9c452f1cb0abfd209ad06f2e9a2
Max Melcher <Max.Melcher@microsoft.com>
2019-08-30 22:45:21 +0200

    empty webapp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LargeFileExchange.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
